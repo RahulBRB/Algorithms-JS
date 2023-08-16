cf097c604b603b0554150d3328d94d7711e4806a
Rahul Baskey <86495244+RahulBRB@users.noreply.github.com>
2023-08-16 13:43:54 +0530

    Update main.js

--- Algorithms/Reverse String/main.js
+++ Algorithms/Reverse String/main.js
@@ -1,7 +1,13 @@
-function convertCtoF(celsius) {
-  let fahrenheit;
-  fahrenheit = celsius * (9 / 5) + 32;
-  return fahrenheit;
+function reverseString(str) {
+  let emptyArray=[];
+  let length = str.length;
+  for(let i=length-1;i>=0;i--){
+    emptyArray.push(str[i]);
+  }
+
+  let result=emptyArray.join('');
+
+  return result;
 }
 
-convertCtoF(30);
+reverseString("hello");
